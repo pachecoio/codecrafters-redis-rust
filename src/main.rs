ed89c64ba6af239d603c4baa96bd07ab25c76e4f
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2023-03-17 13:04:22 -0400

    Handled connection and streamed back RESP response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -8,8 +9,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                let resp_response = "+Ok\r\n";
+                stream.write(resp_response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
